e5795dfe9caf695477f67f561dd1433252e247da
nonjosh <nonjosh@gmail.com>
2022-08-15 07:51:01 +0000

    add vue-gtag

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: { id: "G-XJ2HP58P4L" }
+});
 
 new Vue({
   vuetify,

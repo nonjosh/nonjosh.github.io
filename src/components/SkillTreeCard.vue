8abf970adeed966fca758ddb0a8627806e958b0c
nonjosh <nonjosh@gmail.com>
2022-09-12 17:50:23 +0000

    use specific icons for each skill tree items

--- src/components/SkillTreeCard.vue
+++ src/components/SkillTreeCard.vue
@@ -26,8 +26,8 @@
         :open.sync="open"
       >
         <template v-slot:prepend="{ item }">
-          <v-icon v-if="item.children">mdi-folder-network</v-icon>
-          <v-icon v-else>mdi-code-tags</v-icon>
+          <v-icon v-if="item.icon">{{ item.icon }}</v-icon>
+          <v-icon v-else> mdi-code-tags </v-icon>
         </template>
       </v-treeview>
     </v-card-text>
@@ -42,10 +42,12 @@ export default {
       {
         id: 1,
         name: "Frontend",
+        icon: "mdi-web",
         children: [
           {
             id: 10,
             name: "⭐VueJS",
+            icon: "mdi-vuejs",
             children: [
               {
                 id: 101,
@@ -62,10 +64,12 @@ export default {
       {
         id: 2,
         name: "Backend",
+        icon: "mdi-server",
         children: [
           {
             id: 20,
             name: "⭐Python",
+            icon: "mdi-language-python",
             children: [
               {
                 id: 201,
@@ -80,16 +84,19 @@ export default {
           {
             id: 21,
             name: "Java",
+            icon: "mdi-language-java",
             children: [{ id: 211, name: "Spring" }],
           },
           {
             id: 22,
             name: "Go",
+            icon: "mdi-language-go",
             children: [{ id: 221, name: "Echo" }],
           },
           {
             id: 23,
             name: "NodeJS",
+            icon: "mdi-language-javascript",
             children: [{ id: 231, name: "ExpressJS" }],
           },
         ],
@@ -97,47 +104,57 @@ export default {
       {
         id: 4,
         name: "Database",
+        icon: "mdi-database",
         children: [
           {
             id: 40,
             name: "⭐RDBMS",
+            icon: "mdi-folder",
             children: [
               {
                 id: 401,
                 name: "Oracle",
+                icon: "mdi-database",
               },
               {
                 id: 402,
                 name: "MySQL",
+                icon: "mdi-database",
               },
               {
                 id: 403,
                 name: "SQLite",
+                icon: "mdi-database",
               },
               {
                 id: 404,
                 name: "PostgreSQL",
+                icon: "mdi-database",
               },
             ],
           },
           {
             id: 41,
             name: "NoSQL",
-            children: [{ id: 411, name: "MongoDB" }],
+            icon: "mdi-folder",
+            children: [{ id: 411, name: "MongoDB", icon: "mdi-database" }],
           },
           {
             id: 42,
             name: "MinIO",
+            icon: "mdi-database",
           },
         ],
       },
       {
         id: 3,
         name: "Intrastructure and others",
+        icon: "mdi-server-network",
         children: [
           {
             id: 30,
             name: "IaC",
+            icon: "mdi-folder",
             children: [
               {
                 id: 301,
@@ -152,26 +169,32 @@ export default {
           {
             id: 31,
             name: "Docker",
+            icon: "mdi-docker",
           },
           {
             id: 32,
             name: "Kubernetes",
+            icon: "mdi-kubernetes",
           },
           {
             id: 33,
             name: "Web Servers",
+            icon: "mdi-server-network",
             children: [
               {
                 id: 331,
                 name: "Nginx",
+                icon: "mdi-server-network",
               },
               {
                 id: 332,
                 name: "Apache",
+                icon: "mdi-server-network",
               },
               {
                 id: 333,
                 name: "IIS",
+                icon: "mdi-server-network",
               },
             ],
           },
